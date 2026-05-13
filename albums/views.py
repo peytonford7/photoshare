6b62b7b7afaf97340479fa01afc135e447d01729
Peyton Ford <peytonford7@gmail.com>
2026-05-13 15:36:24 -0600

    Home Layout

--- albums/views.py
+++ albums/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def album_list(request):
+    return render(request, 'albums/album_list.html')
+
+def album_detail(request, pk):
+    return render(request, 'albums/album_detail.html', {'pk': pk})
--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def photo_list(request):
+    return render(request, 'photos/photo_list.html')
+
+def photo_detail(request, pk):
+    return render(request, 'photos/photo_detail.html', {'pk': pk})
